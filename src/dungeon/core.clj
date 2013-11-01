9a6372c91bf6163d903c6798c952629ecaf65137
Kirill Maksimov <k.maksimov@applicaster.com>
2013-11-01 18:46:19 +0200

    I can move!

--- src/dungeon/core.clj
+++ src/dungeon/core.clj
@@ -2,12 +2,42 @@
   (import java.nio.charset.Charset
           com.googlecode.lanterna.TerminalFacade
           com.googlecode.lanterna.terminal.Terminal
-          com.googlecode.lanterna.terminal.Terminal$Color)
+          com.googlecode.lanterna.terminal.Terminal$Color
+          com.googlecode.lanterna.screen.ScreenCharacterStyle
+          com.googlecode.lanterna.input.Key$Kind)
   (:gen-class))
 
 (def terminal (TerminalFacade/createTerminal System/in System/out (Charset/forName "UTF8")))
 
 (def screen (TerminalFacade/createScreen terminal))
+
+(def draw-char
+  (fn [x y]
+    (.putString screen x y "@" Terminal$Color/GREEN Terminal$Color/BLACK #{ScreenCharacterStyle/Bold
+                                                                            ScreenCharacterStyle/Blinking})))
+
+(def read-input-loop
+  (fn []
+    (let [key (.readInput terminal)]
+      (if (nil? key) (recur) key))))
+
+(def walk
+  (fn [x y]
+    (.clear screen)
+    (draw-char x y)
+    (.refresh screen)
+
+    (let [key (read-input-loop)
+          kind (str (.getKind key))]
+
+      (case kind
+        "ArrowDown" (recur x (+ y 1))
+        "ArrowUp" (recur x (- y 1))
+        "ArrowLeft" (recur (- x 1) y)
+        "ArrowRight" (recur(+ x 1) y)
+        (do
+          (println "Unrecognizable input")
+          (recur x y))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -15,7 +45,5 @@
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
 
-
   (.startScreen screen)
-  (.putString screen 10 5 "Welcome to the jungle!" Terminal$Color/WHITE Terminal$Color/BLACK #{})
-  (.refresh screen))
+  (walk 10 15))
