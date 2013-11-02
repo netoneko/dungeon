a4a61db0b148aa2008fd14493893b1c1cdecc269
Kirill Maksimov <k.maksimov@applicaster.com>
2013-11-02 03:13:52 +0200

    Add world!

--- src/dungeon/core.clj
+++ src/dungeon/core.clj
@@ -53,57 +53,73 @@
   (fn [hero args]
     (merge hero {:coords (first args) :direction (second args)})))
 
+(def merge-world
+  (fn [world old-coords hero]
+    (let [world-map (dissoc (world :map ) old-coords)]
+      (merge world {:map (assoc world-map (hero :coords ) hero)
+                    :heroes (vals world-map)}))))
+
 (def human-turn
-  (fn [hero]
+  (fn [world hero]
     (let [coords (hero :coords )
           direction (hero :direction )
           key (read-input-loop)
           kind (str (.getKind key))
-          value (str (.getCharacter key))]
+          value (str (.getCharacter key))
+          new-hero (merge-hero hero (case kind
+                                      "ArrowDown" (list (shift-coords coords :down ) :down )
+                                      "ArrowUp" (list (shift-coords coords :up ) :up )
+                                      "ArrowLeft" (list (shift-coords coords :left ) :left )
+                                      "ArrowRight" (list (shift-coords coords :right ) :right )
+                                      "NormalKey" (do
+                                                    (case value
+                                                      "f" (draw-fireball coords direction)
+                                                      (println "I don't know about this hotkey"))
+                                                    (list coords direction))
+                                      (do
+                                        (println "Unrecognizable input")
+                                        (list coords direction))))]
+      (list (merge-world world (hero :coords ) new-hero) new-hero))))
 
-      (merge-hero hero (case kind
-                         "ArrowDown" (list (shift-coords coords :down ) :down )
-                         "ArrowUp" (list (shift-coords coords :up ) :up )
-                         "ArrowLeft" (list (shift-coords coords :left ) :left )
-                         "ArrowRight" (list (shift-coords coords :right ) :right )
-                         "NormalKey" (do
-                                       (case value
-                                         "f" (draw-fireball coords direction)
-                                         (println "I don't know about this hotkey"))
-                                       (list coords direction))
-                         (do
-                           (println "Unrecognizable input")
-                           (list coords direction)))))))
+(def distance
+  "Euclidean distance between 2 points"
+  (fn [[x1 y1] [x2 y2]]
+    (Math/sqrt
+      (+ (Math/pow (- x1 x2) 2)
+        (Math/pow (- y1 y2) 2)))))
+
+(def observe
+  (fn [world hero]
+    (let [coords (hero :coords )
+          enemies (dissoc (world :heroes ))]
+      (sort #(distance (%1 :coords ) (%2 :coords )) enemies))))
 
 (def ai-turn
-  (fn [hero]
-    (def direction (rand-nth (list :up :down :left :right )))
-    (merge-hero hero (list (shift-coords (hero :coords ) direction) direction))))
+  (fn [world hero]
+    (let [direction (rand-nth (list :up :down :left :right ))
+          new-hero (merge-hero hero
+                     (list (shift-coords (hero :coords ) direction) direction))]
+      (println (str (new-hero :icon ) "'s target is " ((first (observe world hero)) :icon )))
+      (list (merge-world world (hero :coords ) new-hero) new-hero))))
 
-(def walk
-  (fn [hero]
-    (let [hero (if (= (hero :status ) :human )
-                 (human-turn hero)
-                 (ai-turn hero))]
+(def turn
+  (fn [world hero]
+    (let [results (if (= (hero :status ) :human )
+                    (human-turn world hero)
+                    (ai-turn world hero))
+          world (first results)
+          hero (second results)]
       (draw-hero hero)
       (.refresh screen)
-      hero)))
+      world)))
 
 (def notice
   (fn [text]
     (.putString screen 1 0 text Terminal$Color/RED Terminal$Color/BLACK #{ScreenCharacterStyle/Bold})))
 
-(def turn
-  (fn [hero]
-    (walk hero)))
-
 (def game-loop
-  (fn [hero enemies]
-    (let [hero (turn hero)
-          enemies (map turn enemies)]
-      (println hero)
-      (println enemies)
-      (recur hero enemies))))
+  (fn [world]
+    (recur (reduce turn world (world :heroes )))))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -112,17 +128,21 @@
   (alter-var-root #'*read-eval* (constantly false))
 
   (.startScreen screen)
-  (notice "You are in the dungeon. Move with arrows, throw fireballs with 'f'.")
+  (notice "You are in the dungeon. Move with arrows, throw fireballs with 'f'. Press 'space' to start.")
+  (.refresh screen)
 
   (def hero {:coords (list 10 15), :direction :right, :icon "@", :status :human,
              :color Terminal$Color/YELLOW, :background-color Terminal$Color/BLACK
              :screen-settings #{ScreenCharacterStyle/Bold}})
   (def orc {:coords (list 20 20), :direction :right, :icon "B", :status :ai
-            :color Terminal$Color/GREEN, :background-color Terminal$Color/BLACK
+            :color Terminal$Color/RED, :background-color Terminal$Color/BLACK
             :screen-settings #{ScreenCharacterStyle/Bold}})
+  (def troll {:coords (list 15 25), :direction :right, :icon "T", :status :ai
+              :color Terminal$Color/GREEN, :background-color Terminal$Color/BLACK
+              :screen-settings #{ScreenCharacterStyle/Bold}})
 
-  (draw-hero hero)
-  (draw-hero orc)
-  (.refresh screen)
+  (def heroes (list hero orc troll))
 
-  (game-loop hero (list orc)))
+  (def world {:heroes heroes :map (reduce #(assoc %1 (%2 :coords ) %2) {} heroes)})
+
+  (game-loop world))
